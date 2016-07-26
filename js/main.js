eb230e17f0c1db27460c4eebbbc35280f339ea42
Versayr <themoose3@gmail.com>
2016-07-26 14:49:00 -0400

    Shaking off the rust

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+var range = undefined;
+var quantity = undefined;
 
+$(document).ready(function() {
+  $('.button#next').click(function() {
+    range = $('input#range').val();
+    quantity = $('input#quantity').val();
+    console.log('Range is ' + range);
+    console.log('Quantity is ' + quantity);
+  });
+});
