e33d88d95fb987ed88492cc3172f677003c82f92
Versayr <themoose3@gmail.com>
2016-07-27 21:36:47 -0400

    Last commit of the night
    Gotta sleep sometimes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var range = undefined;
 var quantity = undefined;
 
+// A function that takes a month and gives a number of days to put in the
+// #rangeDay selector
+function howManyDays(month) {
+  alert('How many days are in ' + month + '?');
+};
+
 $(document).ready(function() {
   $('.button#next').click(function() {
     // Maybe look at the selector that isn't hidden?
@@ -8,6 +14,7 @@ $(document).ready(function() {
     // MAYBE PUT A MARKER ON THE CURRENT SELECTOR
     if ($('#rangeMonth').find(':selected').text() != 'Month') {
       // alert('Show dates selector for that month, lock month selector');
+      howManyDays($('#rangeMonth').find(':selected').text());
       $('#rangeDay').toggleClass('hidden');
       $('#back').toggleClass('hidden');
       
