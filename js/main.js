75f03b23b3b19451db899d4956b046ac882f2367
Versayr <themoose3@gmail.com>
2016-07-27 21:23:26 -0400

    Working on date selection
    Still shaking off that rust

--- js/main.js
+++ js/main.js
@@ -3,9 +3,15 @@ var quantity = undefined;
 
 $(document).ready(function() {
   $('.button#next').click(function() {
-    range = $('input#range').val();
-    quantity = $('input#quantity').val();
-    console.log('Range is ' + range);
-    console.log('Quantity is ' + quantity);
+    // Maybe look at the selector that isn't hidden?
+    // Or something like that.
+    // MAYBE PUT A MARKER ON THE CURRENT SELECTOR
+    if ($('#rangeMonth').find(':selected').text() != 'Month') {
+      // alert('Show dates selector for that month, lock month selector');
+      $('#rangeDay').toggleClass('hidden');
+      $('#back').toggleClass('hidden');
+      
+      // Add a back button
+    };
   });
 });
